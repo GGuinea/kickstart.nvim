c744a5768b4a8911afb2830b8b6fb38dafec4134
gguinea <kamil5176@gmail.com>
2023-08-30 13:21:29 +0200

    mappings

--- init.lua
+++ init.lua
@@ -290,6 +290,18 @@ vim.keymap.set('n', '<C-j>', '<C-w>j', {})
 vim.keymap.set('n', '<C-k>', '<C-w>k', {})
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
 vim.keymap.set('n', '<leader>h', '<cmd>nohlsearch<CR>', {})
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
 -- end of mine keymaps
 
 -- [[ Highlight on yank ]]
